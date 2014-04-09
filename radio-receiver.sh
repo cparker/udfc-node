e4ac69604718847e4a51a06d19c0f2a2951b7376
root <root@raspberrypi.(none)>
2014-04-09 02:15:53 +0000

    full paths in the radio receiver script

--- radio-receiver.sh
+++ radio-receiver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rtl_fm -p 65 -M fm -f 169500000 -s24k -l 50 | sox  -t raw -r 24k  -es -b16 - -t wav -es -b16 - | minimodem --rx 300 --mark 2133 --space 1920 --binary --quiet -f - | ./alert-iflows-decoder.py --mongo
+/usr/local/bin/rtl_fm -p 65 -M fm -f 169500000 -s24k -l 50 | /usr/bin/sox -t raw -r 24k  -es -b16 - -t wav -es -b16 - | /usr/local/bin/minimodem --rx 300 --mark 2133 --space 1920 --binary --quiet -f - | /udfc/alert-iflows-decoder.py --mongo &> /bigvar/log/rtl-fm-udfc.log &
